e61c80c085f6ef6504704689c92cdf28dc4fc13d
Akash chandra <akash99chandra@gmail.com>
2022-06-08 02:57:34 +0530

    created react app and done clean-up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Building whatsApp clone</h1>
+      <div className="app__body">
+        { /* Sidebar */  }
+        { /* Chat */  }
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAsrAS7dq3AW_A69ez5Z9G82JDBLJ_Uj-c",
+    authDomain: "whatsapp-clone-e4f10.firebaseapp.com",
+    projectId: "whatsapp-clone-e4f10",
+    storageBucket: "whatsapp-clone-e4f10.appspot.com",
+    messagingSenderId: "819638830155",
+    appId: "1:819638830155:web:55cfa5a284ec9e35fcb9ce"
+}
